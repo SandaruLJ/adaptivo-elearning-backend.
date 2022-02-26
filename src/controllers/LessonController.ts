ad28549445b80a5007bbfb7eb78555cb0ec5f1e2
Rishy20 <58569021+Rishy20@users.noreply.github.com>
2022-02-26 19:47:11 +0530

    Implement CRUD for Lesson Management

---
+++ src/controllers/LessonController.ts
@@ -0,0 +1,94 @@
+import {Logger} from "../loaders/logger";
+import {LessonService} from "../services/LessonService";
+import {ILesson} from "../interfaces/ILesson";
+import {ILessonService} from "../services/interfaces/ILessonService";
+const autoBind = require('auto-bind');
+
+
+export default class LessonController{
+
+    private logger:Logger;
+    private LessonService:ILessonService;
+
+    constructor(){
+        this.logger = Logger.getInstance();
+        this.LessonService = LessonService.getInstance();
+        autoBind(this);
+    }
+
+    public async createLesson(req:any,res:any){
+        this.logger.info("LessonController - createLesson()");
+
+        if(req.body){
+
+            const Lesson:ILesson = JSON.parse(req.body.data);
+            await this.LessonService.createLesson(Lesson)
+                .then(data => {
+                    res.status(200).send(data);
+                })
+
+                .catch(error => {
+                    this.logger.error(error.message);
+                    res.status(500).send({err:error.message});
+                })
+        }
+    }
+    public async getAllLessons(req:any,res:any) {
+        this.logger.info("LessonController - getAllLesson()");
+
+        await this.LessonService.getAllLesson()
+            .then(data => {
+                res.status(200).send(data);
+            })
+            .catch(error => {
+                this.logger.error(error.message);
+                res.status(500).send({err: error.message});
+            })
+    }
+    public async getLessonById(req:any,res:any) {
+        this.logger.info("LessonController - getLessonById()");
+        const id = req.params.id;
+        await this.LessonService.getLessonById(id)
+            .then(data => {
+                res.status(200).send(data);
+            })
+            .catch(error => {
+                this.logger.error(error.message);
+                res.status(500).send({err: error.message});
+            })
+    }
+
+    public async updateLesson(req:any,res:any) {
+        this.logger.info("LessonController - updateLesson()");
+        const id = req.params.id;
+
+        if(req.body) {
+
+            const Lesson: ILesson = JSON.parse(req.body.data);
+
+            await this.LessonService.updateLesson(id, Lesson)
+                .then(data => {
+                    res.status(200).send(data);
+                })
+                .catch(error => {
+                    this.logger.error(error.message);
+                    res.status(500).send({err: error.message});
+                })
+        }else {
+            res.status(404);
+        }
+    }
+
+    public async deleteLesson(req:any,res:any) {
+        this.logger.info("LessonController - deleteLesson()");
+        const id = req.params.id;
+        await this.LessonService.deleteLesson(id)
+            .then(data => {
+                res.status(200).send(data);
+            })
+            .catch(error => {
+                this.logger.error(error.message);
+                res.status(500).send({err: error.message});
+            })
+    }
+}
--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -1,11 +1,14 @@
 import * as express from "express";
 import CourseController from "../controllers/CourseController";
 import HealthController from  "../controllers/HealthController";
+import LessonController from "../controllers/LessonController";
+
 export default function setRoutes(app:any){
 
     const router = express();
     const courseControl = new CourseController();
     const healthControl = new HealthController();
+    const lessonControl = new LessonController();
 
 
     app.use("/api",router);
@@ -19,5 +22,11 @@ export default function setRoutes(app:any){
     router.route('/courses/:id').get(courseControl.getCourseById);
     router.route('/courses/:id').put(courseControl.updateCourse);
     router.route('/courses/:id').delete(courseControl.deleteCourse);
+
+    router.route('/lessons').post(lessonControl.createLesson);
+    router.route('/lessons').get(lessonControl.getAllLessons);
+    router.route('/lessons/:id').get(lessonControl.getLessonById);
+    router.route('/lessons/:id').put(lessonControl.updateLesson);
+    router.route('/lessons/:id').delete(lessonControl.deleteLesson);
 
 }
