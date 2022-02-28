b5c0e9025351ea4abeb8a0e2a6a9d7c7635a0a0e
Piumi <piumiekanayaka4@gmail.com>
2022-03-01 00:05:03 +0530

    Create enpointd for category management

---
+++ src/interfaces/ICategory.ts
@@ -0,0 +1,9 @@
+export interface ICategory{
+    title:string;
+    description:string;
+    image:string;
+    course:string[];
+
+
+
+}
---
+++ src/models/Category.ts
@@ -0,0 +1,32 @@
+import {ICategory} from "../interfaces/ICategory";
+import * as mongoose from "mongoose";
+const Schema = mongoose.Schema;
+const CategorySchema = new Schema(
+    {
+        title:{
+            type:String,
+            required:true,
+            trim:true
+        },
+        description:{
+            type:String,
+            required:true,
+            trim:true,
+        },
+
+        image:{
+            type:String,
+            required:true,
+        },
+        course:[{
+            type:String,
+            required:false,
+            trim:true
+        }]
+
+    }
+
+
+
+);
+export default mongoose.model<ICategory & mongoose.Document>('categories',CategorySchema)
