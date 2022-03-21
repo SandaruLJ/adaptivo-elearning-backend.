b65af1f9275cd90fd395fe0f3036033c8c6f9252
jayani wickramathilaka <Jayani Saumya>
2022-03-22 01:25:18 +0530

    rename to quiz

---
+++ src/controllers/QuizController.ts
@@ -0,0 +1,85 @@
+import {Logger} from "../loaders/logger";
+import {QuizService} from "../services/QuizService";
+import {IQuiz} from "../interfaces/IQuiz";
+import {IQuizService} from "../services/interfaces/IQuizService";
+const autoBind = require('auto-bind');
+
+export default class QuizController{
+
+    private logger:Logger;
+    private quizService:IQuizService
+
+    constructor(){
+        this.logger = Logger.getInstance();
+        this.quizService = QuizService.getInstance();
+        autoBind(this);
+    }
+
+    public async createQuiz(req:any,res:any){
+        this.logger.info("QuizController - createQuiz()");
+
+        if(req.body){
+            await this.quizService.createQuiz(req.body)
+                .then(data => {
+                    res.status(200).send(data);
+                })
+
+                .catch(error => {
+                    this.logger.error(error.message);
+                    res.status(500).send({err:error.message});
+                })
+        }
+    }
+    public async getAllQuiz(req:any,res:any) {
+        this.logger.info("QuizController - getAllQuiz()");
+
+        await this.quizService.getAllQuiz()
+            .then(data => {
+                res.status(200).send(data);
+            })
+            .catch(error => {
+                this.logger.error(error.message);
+                res.status(500).send({err: error.message});
+            })
+    }
+    public async getQuizById(req:any,res:any) {
+        this.logger.info("QuizController - getQuizById()");
+        const id = req.params.id;
+        await this.quizService.getQuizById(id)
+            .then(data => {
+                res.status(200).send(data);
+            })
+            .catch(error => {
+                this.logger.error(error.message);
+                res.status(500).send({err: error.message});
+            })
+    }
+
+    public async updateQuiz(req:any,res:any) {
+        this.logger.info("QuizController - updateQuiz()");
+
+        const id = req.params.id;
+        const quiz:IQuiz = req.body;
+        await this.quizService.updateQuiz(id , quiz)
+            .then(data => {
+                res.status(200).send(data);
+            })
+            .catch(error => {
+                this.logger.error(error.message);
+                res.status(500).send({err: error.message});
+            })
+    }
+
+    public async deleteQuiz(req:any,res:any) {
+        this.logger.info("QuizController - deleteQuiz()");
+        const id = req.params.id;
+        await this.quizService.deleteQuiz(id)
+            .then(data => {
+                res.status(200).send(data);
+            })
+            .catch(error => {
+                this.logger.error(error.message);
+                res.status(500).send({err: error.message});
+            })
+    }
+}
---
+++ src/interfaces/IQuiz.ts
@@ -0,0 +1,8 @@
+export interface IQuiz {
+    _id?: string;
+    quiz: string;
+    answers:String;
+    correctAnswer:String;
+    LO_id:Number
+};
+
