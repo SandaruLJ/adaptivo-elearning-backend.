78a9ff685b4887a96321b32c485974e959cd885d
Sandaru Jayasekara <sandarulakshitha1999@gmail.com>
2022-05-05 16:15:55 +0000

    Implement learning path generation algorithm

---
+++ src/recommendation/learning_resources.ts
@@ -0,0 +1,132 @@
+let learning_resources = [
+    {
+        _id: "1",
+        loId: "6",
+        type: "video",
+    },
+    {
+        _id: "2",
+        loId: "6",
+        type: "audio",
+    },
+    {
+        _id: "3",
+        loId: "6",
+        type: "text",
+    },
+    {
+        _id: "4",
+        loId: "7",
+        type: "video",
+    },
+    {
+        _id: "5",
+        loId: "7",
+        type: "audio",
+    },
+    {
+        _id: "6",
+        loId: "7",
+        type: "text",
+    },{
+        _id: "7",
+        loId: "8",
+        type: "video",
+    },
+    {
+        _id: "8",
+        loId: "8",
+        type: "audio",
+    },
+    {
+        _id: "9",
+        loId: "8",
+        type: "text",
+    },{
+        _id: "10",
+        loId: "9",
+        type: "video",
+    },
+    {
+        _id: "11",
+        loId: "9",
+        type: "audio",
+    },
+    {
+        _id: "12",
+        loId: "9",
+        type: "text",
+    },{
+        _id: "13",
+        loId: "10",
+        type: "video",
+    },
+    {
+        _id: "14",
+        loId: "10",
+        type: "audio",
+    },
+    {
+        _id: "15",
+        loId: "10",
+        type: "text",
+    },{
+        _id: "16",
+        loId: "11",
+        type: "video",
+    },
+    {
+        _id: "17",
+        loId: "11",
+        type: "audio",
+    },
+    {
+        _id: "18",
+        loId: "11",
+        type: "text",
+    },{
+        _id: "19",
+        loId: "12",
+        type: "video",
+    },
+    {
+        _id: "20",
+        loId: "12",
+        type: "audio",
+    },
+    {
+        _id: "21",
+        loId: "12",
+        type: "text",
+    },{
+        _id: "22",
+        loId: "13",
+        type: "video",
+    },
+    {
+        _id: "23",
+        loId: "13",
+        type: "audio",
+    },
+    {
+        _id: "24",
+        loId: "13",
+        type: "text",
+    },{
+        _id: "25",
+        loId: "14",
+        type: "video",
+    },
+    {
+        _id: "26",
+        loId: "14",
+        type: "audio",
+    },
+    {
+        _id: "27",
+        loId: "14",
+        type: "text",
+    }
+]
+
+export let getLearningResources = () => new Promise(resolve => resolve(learning_resources));
